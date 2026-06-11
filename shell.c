d46a73a419f87364fdcb23a30cb6c493775c894b
Tanishqkumarpatel <tanishq38123@gmail.com>
2026-06-10 23:24:49 -0700

    Add shell command execution and exit helper

--- shell.c
+++ shell.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define BUFFER_CMD_SIZE 1024
 #define BUFFER_ARGS_SIZE 10
 #define RED "\033[0;31m"
 #define RESET "\e[0m"
 #define SHELL_DELIM " \t\r\n"
-
 
 char*   read_command();
 char**  get_arguments(char *);
@@ -52,28 +54,50 @@ char** get_arguments(char *cmd) {
     int position = 0;
     char **args = malloc(buffer_size * sizeof(char *));
     if (!args) {
-        fprintf(stderr, "%smy_shell: Argument Allocation Error%s", RED, RESET);
+        fprintf(stderr, "%smy_shell: Argument Allocation Error%s\n", RED, RESET);
         exit(EXIT_FAILURE);
     }
     char *token = strtok(cmd, SHELL_DELIM);
 
     while (token != NULL) {
         args[position++] = token;
-        printf("%s\n", token);
 
         if (position >= buffer_size) {
             buffer_size += BUFFER_ARGS_SIZE;
             args = realloc(args, buffer_size * sizeof(char *));
 
             if (!args) {
-                fprintf(stderr, "%smy_shell: Argument Allocation Error%s", RED, RESET);
+                fprintf(stderr, "%smy_shell: Argument Allocation Error%s\n", RED, RESET);
                 exit(EXIT_FAILURE);
             }
         }
 
         token = strtok(NULL, SHELL_DELIM);
     }  
+    args[position] = NULL;
     return args;
+}
+
+int execute(char **args) {
+    if (args[0] == NULL) return 1;
+    if (strcmp(args[0], "exit") == 0) {
+        return 0;
+    }
+    int status;
+    pid_t cpid = fork();
+
+    if (cpid == 0) {
+        if (execvp(args[0], args) < 0) {
+            perror("my_shell");
+            exit(EXIT_FAILURE);
+        }
+    } else if (cpid < 0) {
+        printf("%smy_shell: Error Forking Process%s\n", RED, RESET);
+        exit(EXIT_FAILURE);
+    } else {
+        waitpid(cpid, &status, WUNTRACED);
+    }
+    return 1;
 }
 
 void run() {
@@ -86,9 +110,9 @@ void run() {
         printf("my_shell>>> ");
         cmd    = read_command();
         args   = get_arguments(cmd);
-        // status = execute(args);
-        free(cmd);
+        status = execute(args);
         free(args);
+        free(cmd);
     }
     
 
