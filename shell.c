7eea1f4b27a1ae02a4a0c190c9b5e63d4309dd35
Tanishqkumarpatel <tanishq38123@gmail.com>
2026-06-10 21:58:29 -0700

    Add read command helper

--- shell.c
+++ shell.c
@@ -2,11 +2,47 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define BUFFER_CMD_SIZE 1024
+#define RED "\033[0;31m"
+#define RESET "\e[0m"
+
 char*   read_command();
 char**  get_arguments(char *);
 int     execute(char **);
 
+char* read_command() {
+    
+    int position = 0;
+    int c;
+    int buffer_size = BUFFER_CMD_SIZE;
+    char *buffer = malloc(buffer_size);
 
+    if (!buffer) {
+        fprintf(stderr, "%smy_shell: Allocation error%s\n", RED, RESET);
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        c = getchar();
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= buffer_size) {
+            buffer_size += BUFFER_CMD_SIZE;
+            buffer = realloc(buffer, buffer_size);
+
+            if (!buffer) {
+                fprintf(stderr, "%smy_shell: Allocation error%s\n", RED, RESET);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
 
 void run() {
     char  *cmd   = NULL;
@@ -17,8 +53,9 @@ void run() {
     {
         printf("my_shell>>> ");
         cmd    = read_command();
-        args   = get_arguments(cmd);
-        status = execute(args);
+        printf("%s\n", cmd);
+        // args   = get_arguments(cmd);
+        // status = execute(args);
         free(cmd);
         free(args);
     }
